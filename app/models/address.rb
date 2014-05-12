c20ff9be116248079c602325477ead592f7eb2dd
djotterfield <adb707@yahoo.com>
2014-05-12 09:46:42 -0700

    Refactor Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,18 +1,19 @@
 class Address < ActiveRecord::Base
   belongs_to :person
 
-  validates :street, presence: true
-  validates :city, presence: true
-  validates :zipcode, presence: true
+  validates :street, :city, :zipcode, presence: true
 
-  before_save :set_default_country, if: :country_nil? 
+  before_save :set_default_country, if: :country_field_is_blank? 
 
-  def country_nil?
-    self.country.nil?
-  end
 
-  def set_default_country
-    self.country = "USA"
-  end
+  private
+    def country_field_is_blank?
+      #use :blank? rather than :nil?, because :blank? checks for both nil and empty string
+      self.country.blank?
+    end
+
+    def set_default_country
+      self.country = "USA"
+    end
 
 end
