0f8f0f9cf682b30fe1f88f4ea5bc395583ef11d8
djotterfield <adb707@yahoo.com>
2014-05-12 09:42:15 -0700

    Refactor Address spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -5,29 +5,26 @@ describe Address do
     FactoryGirl.create(:address).should be_valid
   end
 
-  before do 
-    p = FactoryGirl.create(:person)
-  end  
-
-  it "must have a street" do
-    FactoryGirl.build(:address, :street => nil).should_not be_valid
-  end
-
-  it "must have a city" do
-    FactoryGirl.build(:address, :city => nil).should_not be_valid
-  end 
-
-  it "must have a zipcode" do
-    FactoryGirl.build(:address, :zipcode => nil).should_not be_valid
+  # before do 
+  #   p = FactoryGirl.create(:person)
+  # end  
+  context "Validations" do
+    [:street, :city, :zipcode].each do |attr|
+      it "must have a #{attr}" do
+        FactoryGirl.build(:address, attr => nil).should_not be_valid
+      end
+    end
   end
 
   it "isn't required to have a country" do
     FactoryGirl.build(:address, :country => nil).should be_valid
   end
 
-  it "should have country default to 'USA' if left blank" do
-    address = FactoryGirl.build(:address, :country => nil)
-    address.save
-    address.country.should == "USA"
+  context "when country field is missing" do
+    it "should default to 'USA'" do
+      address = FactoryGirl.build(:address, :country => nil)
+      address.save
+      address.country.should == "USA"
+    end
   end
 end
