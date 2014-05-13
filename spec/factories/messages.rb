9e9fe40470a12ed9d8469ad939b7214a323d2a58
djotterfield <adb707@yahoo.com>
2014-05-12 22:25:21 -0700

    Make message factory

---
+++ spec/factories/messages.rb
@@ -0,0 +1,12 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
+
+FactoryGirl.define do
+  factory :message do |m|
+    m.subject { Faker::Lorem.sentence(word_count = 4, supplemental = false, random_words_to_add = 6) }
+    m.body { Faker::Lorem.paragraph(sentence_count = 3, supplemental = false, random_sentences_to_add = 3) }
+    m.read_at "2014-05-12 21:07:48"
+    m.association :sender, factory: :person
+    m.association :recipient, factory: :person
+  end
+end
