5f0626d4ce2fa624b6360bcfe0369cc353346816
djotterfield <adb707@yahoo.com>
2014-05-13 13:10:58 -0700

    Create items model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513193650) do
+ActiveRecord::Schema.define(version: 20140513200426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20140513193650) do
   end
 
   add_index "addresses", ["person_id"], name: "index_addresses_on_person_id", using: :btree
+
+  create_table "items", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.decimal  "price",       precision: 10, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "messages", force: true do |t|
     t.string   "subject"
