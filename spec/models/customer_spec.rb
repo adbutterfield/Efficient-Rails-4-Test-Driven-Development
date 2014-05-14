e1d71f347a6d3a06c36ec2b810f35a537d795181
djotterfield <adb707@yahoo.com>
2014-05-14 11:04:23 -0700

    Build and test customer factory

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Customer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid factory" do
+    FactoryGirl.create(:customer).should be_valid
+  end
+
+  it "should be a type of person" do
+    FactoryGirl.create(:customer).should be_kind_of(Person)
+  end
 end
