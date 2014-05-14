331a155154ac89088f4988cb4495e4ddf5fd990d
djotterfield <adb707@yahoo.com>
2014-05-13 21:14:30 -0700

    Refactor factories

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     m.subject { Faker::Lorem.sentence(word_count = 4, supplemental = false, random_words_to_add = 6) }
     m.body { Faker::Lorem.paragraph(sentence_count = 3, supplemental = false, random_sentences_to_add = 3) }
     # m.read_at "2014-05-12 21:07:48"
-    m.association :sender, factory: :person
-    m.association :recipient, factory: :person
+    sender
+    recipient
   end
 end
