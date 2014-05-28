1ddc6f80c9f91a60bdea8867f564f6efe822a688
djotterfield <adb707@yahoo.com>
2014-05-28 00:59:41 -0700

    Add id to items_orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140514174343) do
+ActiveRecord::Schema.define(version: 20140528011318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20140514174343) do
     t.datetime "updated_at"
   end
 
-  create_table "items_orders", id: false, force: true do |t|
+  create_table "items_orders", force: true do |t|
     t.integer "item_id"
     t.integer "order_id"
   end
